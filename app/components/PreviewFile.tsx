b2d664e01515ea87566a7589722b0c1e473499d7
jimamuto <onyangojimmy2005@gmail.com>
2026-04-21 01:10:22 +0300

    feat:add preview by mapping values

---
+++ app/components/PreviewFile.tsx
@@ -0,0 +1,8 @@
+const PreviewFile = ({date,title,description}) =>{
+return(
+<div>
+<span className="text-base">{date}</span>
+<h1 className="text-xl font-bold">{title}</h1>
+<p className="text-gray-500">{description}</p>
+</div>)}
+export default PreviewFile
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-const page =()=>{
-return(
-<div> <h1>Jim's Developer Journey</h1><p> I am a He/him living in Nairobi Kenya and in this blog is where i express myself</p>
-  </div>
-)}
+import PreviewFile from "@/app/components/PreviewFile";
+export default function page (){
+  
+  const files = [
+    {id: 1, date: "April 20", title: "Minimax", description: "How to use minimax in coding" },
+    { id: 2, date: "April 21", title: "Developer Journey", description: "How i have improved in developing" },
+    { id: 3, date: "April 22", title: "Homelab", description: "Take a peep on my homelabbing journey" },
+  ];
 
-export default page
+return(
+<div> <h1>Jim's Developer Journey</h1>
+<p> I am a He/him living in Nairobi Kenya and in this blog is where i express myself</p>
+{files.map((file)=>(
+  <PreviewFile 
+  key={file.id}
+  date={file.date}
+  description={file.description}
+  />
+  ))}
+  </div>
+);
+}
+
